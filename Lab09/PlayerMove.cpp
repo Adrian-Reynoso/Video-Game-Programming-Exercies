36a6a8f9e325845d7bbc59ffec3853e128c733d4
Adrian Reynoso <adrianreynoso@Adrians-MacBook-Pro.local>
2022-03-29 14:41:55 -0700

    Working Part 2 of Lab09

--- Lab09/PlayerMove.cpp
+++ Lab09/PlayerMove.cpp
@@ -12,16 +12,30 @@
 #include "CollisionComponent.h"
 #include "Random.h"
 #include "CameraComponent.hpp"
+#include "Block.hpp"
 
 PlayerMove::PlayerMove(class Player* owner)
 : MoveComponent(owner)
 {
     mPlayer = owner;
+    ChangeState(Falling);
 }
 
 void PlayerMove::Update(float deltaTime)
 {
-    MoveComponent::Update(deltaTime);
+    //Check current state and call it's respective update function
+    if (mCurrentState == OnGround)
+    {
+        UpdateOnGround(deltaTime);
+    }
+    else if (mCurrentState == Jump)
+    {
+        UpdateJump(deltaTime);
+    }
+    else
+    {
+        UpdateFalling(deltaTime);
+    }
 }
 
 void PlayerMove::ProcessInput(const Uint8* keyState)
@@ -54,6 +68,13 @@ void PlayerMove::ProcessInput(const Uint8* keyState)
         SetStrafeSpeed(0.0f);
     }
     
+    //For jumping
+    if (keyState[SDL_SCANCODE_SPACE] && !spaceWasPressed && mCurrentState == OnGround)
+    {
+        mZSpeed = JUMP_SPEED;
+        ChangeState(Jump);
+    }
+    
     //Relative Mouse Movement
     int x, y;
     SDL_GetRelativeMouseState(&x, &y);
@@ -65,5 +86,117 @@ void PlayerMove::ProcessInput(const Uint8* keyState)
     //Calculate the angular speed and pass into MoveComponent
     float pitchSpeed = (y / 500.0f) * Math::Pi * 10.0f;
     mPlayer->cameraComponent->SetPitchSpeed(pitchSpeed);
+    
+    //For leading edge detection
+    spaceWasPressed = keyState[SDL_SCANCODE_SPACE];
 }
 
+void PlayerMove::ChangeState(MoveState state)
+{
+    mCurrentState = state;
+}
+
+void PlayerMove::UpdateOnGround(float deltaTime)
+{
+    MoveComponent::Update(deltaTime);
+    
+    //Loop over all the blocks in the game and call FixCollision on each
+    bool isOnBlock = false;
+    for (Actor* obstacle : mOwner->GetGame()->GetBlockVector())
+    {
+        Block* otherBlock = (Block*) obstacle;
+        CollSide onBlock = FixCollision(mPlayer->collisionComponent, otherBlock->collisionComponent);
+        
+        //If ANY of the FixCollision calls return Top, this means you landed
+        if (onBlock == CollSide::Top)
+        {
+            isOnBlock = true;
+        }
+    }
+    
+    if (isOnBlock)
+    {
+        ChangeState(Falling);
+    }
+}
+
+void PlayerMove::UpdateJump(float deltaTime)
+{
+    MoveComponent::Update(deltaTime);
+    
+    //Update mZSpeed based on gravity and delta time
+    mZSpeed += GRAVITY * deltaTime;
+    
+    //Update the owner’s z position based on mZSpeed and delta time
+    Vector3 tempPos = mOwner->GetPosition();
+    tempPos.z += mZSpeed * deltaTime;
+    mOwner->SetPosition(tempPos);
+    
+    //Loop over all the blocks in the game and call FixCollision on each
+    for (Actor* obstacle : mOwner->GetGame()->GetBlockVector())
+    {
+        Block* otherBlock = (Block*) obstacle;
+        CollSide onBlock = FixCollision(mPlayer->collisionComponent, otherBlock->collisionComponent);
+        
+        //If ANY of the FixCollision calls return Top, this means you landed
+        if (onBlock == CollSide::Bottom)
+        {
+            mZSpeed = 0.0f;
+            ChangeState(OnGround);
+        }
+    }
+    
+    //If the mZSpeed <= 0.0f. If it is, that means you reached the apex of your jump, and should start falling
+    if (mZSpeed <= 0.0f)
+    {
+        ChangeState(Falling);
+    }
+}
+
+void PlayerMove::UpdateFalling(float deltaTime)
+{
+    MoveComponent::Update(deltaTime);
+    
+    //Update mZSpeed based on gravity and delta time
+    mZSpeed += GRAVITY * deltaTime;
+    
+    //Update the owner’s z position based on mZSpeed and delta time
+    Vector3 tempPos = mOwner->GetPosition();
+    tempPos.z += mZSpeed * deltaTime;
+    mOwner->SetPosition(tempPos);
+    
+    //Loop over all the blocks in the game and call FixCollision on each
+    for (Actor* obstacle : mOwner->GetGame()->GetBlockVector())
+    {
+        Block* otherBlock = (Block*) obstacle;
+        CollSide onBlock = FixCollision(mPlayer->collisionComponent, otherBlock->collisionComponent);
+        
+        //If ANY of the FixCollision calls return Top, this means you landed
+        if (onBlock == CollSide::Top)
+        {
+            mZSpeed = 0.0f;
+            ChangeState(OnGround);
+        }
+    }
+}
+
+CollSide PlayerMove::FixCollision(class CollisionComponent* self, class CollisionComponent* block)
+{
+    //Get current position
+    Vector3 tempPos = mOwner->GetPosition();
+    
+    //Call GetMinOverlap(), If side isn’t CollSide::None fix the position of the player based on offset.
+        Vector3 offSet {0.0f, 0.0f, 0.0f};
+    CollSide onBlock = mPlayer->collisionComponent->GetMinOverlap(block, offSet);
+
+        if (onBlock != CollSide::None)
+        {
+            //Add offset to temPos
+            tempPos += offSet;
+            
+            //Call setPosition
+            mOwner->SetPosition(tempPos);
+        }
+    
+    return onBlock;
+}
--- Lab09/PlayerMove.hpp
+++ Lab09/PlayerMove.hpp
@@ -13,6 +13,7 @@
 #include "MoveComponent.h"
 #include <vector>
 #include "SDL2/SDL_mixer.h"
+#include "CollisionComponent.h"
 
 class PlayerMove : public MoveComponent
 {
@@ -20,6 +21,21 @@ class PlayerMove : public MoveComponent
     PlayerMove(class Player* owner);
     void Update(float deltaTime) override;
     void ProcessInput(const Uint8* keyState) override;
+    void UpdateOnGround(float deltaTime);
+    void UpdateJump(float deltaTime);
+    void UpdateFalling(float deltaTime);
+    
+    enum MoveState
+    {
+        OnGround,
+        Jump,
+        Falling
+    };
+    
+    void ChangeState(MoveState state);
+    
+    protected:
+    CollSide FixCollision(class CollisionComponent* self, class CollisionComponent* block);
     
     private:
     //Private functions for updating game
@@ -30,5 +46,10 @@ class PlayerMove : public MoveComponent
     std::vector<int> sideIndexPattern {0, 1, 2, 0};
     std::vector<int> topIndexPattern {6, 7};
     float forwardSpeed = 350.0f;
+    MoveState mCurrentState;
+    float mZSpeed = 0.0f;
+    const float GRAVITY = -980.0f;
+    const float JUMP_SPEED = 500.0f;
+    bool spaceWasPressed = false;
 };
 #endif /* PlayerMove_hpp */
